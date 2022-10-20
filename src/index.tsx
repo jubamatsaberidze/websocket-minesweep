da4c3224e885b1f689b86687f911b802a2ac3765
jubamatsaberidze <j.matsaberidze.websmart@gmail.com>
2022-10-20 18:41:35 +0400

    added provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './app/App';
+import { Provider } from 'react-redux';
+import store from './store/store';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
 
 reportWebVitals();
